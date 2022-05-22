6e6643d7d6787c276b5e4d72fc408004937f92a2
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2022-05-22 23:03:36 +0300

    Pricing kısmının yapıldı

---
+++ where/src/pages/Pricing/Pricing.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import "../../css/style.css"
+const Pricing = () => {
+    return (
+        <div role="tabpanel" className="tab-pane" id="reviews">
+
+            <ul class="prices">
+                <li>
+                    <div class="pricebox">
+                        <h3>Chicken Corn Soup</h3>
+                        <p>Served with Fish Crackers (Individual)</p>
+                        <span class="price">$12</span>
+                    </div>
+                </li>
+                <li>
+                    <div class="pricebox">
+                        <h3>Chicken Corn Soup</h3>
+                        <p>Served with Fish Crackers (Individual)</p>
+                        <span class="price">$12</span>
+                    </div>
+                </li>
+                <li>
+                    <div class="pricebox">
+                        <h3>Masala Fries</h3>
+                        <p>Served with Fish Crackers (Individual)</p>
+                        <span class="price">$5</span>
+                    </div>
+                </li>
+                <li>
+                    <div class="pricebox">
+                        <h3>Chicken Corn Soup</h3>
+                        <p>Served with Fish Crackers (Individual)</p>
+                        <span class="price">$12</span>
+                    </div>
+                </li>
+                <li>
+                    <div class="pricebox">
+                        <h3>Inside Out Sandwich</h3>
+                        <p>Served with Fish Crackers (Individual)</p>
+                        <span class="price">$54</span>
+                    </div>
+                </li>
+                <li>
+                    <div class="pricebox">
+                        <h3>Chicken Corn Soup</h3>
+                        <p>Served with Fish Crackers (Individual)</p>
+                        <span class="price">$12</span>
+                    </div>
+                </li>
+                <li>
+                    <div class="pricebox">
+                        <h3>Chicken Corn Soup</h3>
+                        <p>Served with Fish Crackers (Individual)</p>
+                        <span class="price">$12</span>
+                    </div>
+                </li>
+                <li>
+                    <div class="pricebox">
+                        <h3>Chicken Corn Soup</h3>
+                        <p>Served with Fish Crackers (Individual)</p>
+                        <span class="price">$12</span>
+                    </div>
+                </li>
+                <li>
+                    <div class="pricebox">
+                        <h3>Chicken Corn Soup</h3>
+                        <p>Served with Fish Crackers (Individual)</p>
+                        <span class="price">$12</span>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Pricing
