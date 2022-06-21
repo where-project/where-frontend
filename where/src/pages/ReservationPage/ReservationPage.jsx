ce5bd9d05edcc71c05ecb55f3bae99628819db08
Yasemin Gerboğa <yasemingerboga@gmail.com>
2022-06-21 12:53:20 +0300

    bugfix reservation

--- where/src/pages/ReservationPage/ReservationPage.jsx
+++ where/src/pages/ReservationPage/ReservationPage.jsx
@@ -17,7 +17,7 @@ const ReservationPage = ({ place, user, ...args }) => {
   const [reservations, setReservations] = useState([]);
   const [account, setAccount] = useState("");
   const [userDetails, setUserDetails] = useState({});
-  const [isReserved, setIsReserved] = useState(false);
+  const [successMessage, setSuccessMessage] = useState("")
   let web3;
   let reservationModel = {
     "email": userDetails.email,
@@ -46,8 +46,11 @@ const ReservationPage = ({ place, user, ...args }) => {
       try {
         await window.ethereum.request({ method: "eth_requestAccounts" });
         web3 = new Web3(window.ethereum);
+        const accounts = await web3.eth.getAccounts();
+        setAccount(accounts[0]);
       } catch (error) {
         setError(error.message);
+        setSuccessMessage("");
       }
     } else {
       window.alert("Please install MetaMask!");
@@ -61,7 +64,6 @@ const ReservationPage = ({ place, user, ...args }) => {
       setAccount(accounts[0]);
       const networkId = await web3.eth.net.getId(); //5777
       const networkData = ReservationDetail.networks[networkId];
-
       if (networkData) {
         const reservation = new web3.eth.Contract(
           ReservationDetail.abi,
@@ -102,31 +104,90 @@ const ReservationPage = ({ place, user, ...args }) => {
   const handleTime = (value) => {
     setReservationTime(value);
     reservationModel.reservationTime = value;
+    setError("");
   }
 
+  const checkIfTimeIsValid = (date, time) => {
+    let flag = false;
+    let timeArray = time.split(":");
+    let hour = Number(timeArray[0]);
+    let minute = Number(timeArray[1]);
+    const reservationDateArray = date.toString().split(" ");
+    let day = reservationDateArray[0];
+    place.businessHours.map(businessHour => {
+      const activeDays = businessHour.day.toString().match(/.{1,3}/g);
+      const activeDay = activeDays[0];
+      if (day === activeDay) {
+        const startTime = businessHour.startTime.split(":");
+        const startHour = Number(startTime[0]);
+        const startMinute = Number(startTime[1]);
+        const endTime = businessHour.closingTime.split(":");
+        const endHour = Number(endTime[0]);
+        if (hour >= startHour && hour < endHour) {
+          if (hour === startHour) {
+            if (minute >= startMinute) {
+              flag = true;
+            }
+          }
+          flag = true;
+        }
+        else {
+          flag = false;
+          setError("Place is not open at this time!");
+        }
+      }
+    });
+    return flag;
+  }
+  const checkIfReservedAlready = () => {
+    let flag = true;
+    reservations.map(reservation => {
+      const reservationDateArray = reservation[2].split(" ");
+      const reservationDate = reservationDateArray[0];
+      const reservationTime = reservation[3];
+      console.log(date.toLocaleDateString(), "----", reservationDate);
+      if (reservationDate === date.toLocaleDateString() && reservationTime === time) {
+        flag = false;
+        setError("Place has been reserved at this time! Try another time!");
+      }
+    })
+    return flag;
+  }
   const sendEmail = () => {
     emailService = new EmailService();
     emailService.sendEmail(reservationModel).then((result) => {
-      setIsReserved(true);
+      setError("");
+      setSuccessMessage("Reservation completed! Email sent.");
     }, err => {
       console.log(err.response);
     });
   }
 
   const createReservation = (placeId, date, time) => {
-    const reservationDate = date.toLocaleString();
-    const dateTimeForCompare = new Date(date);
-    if (dateTimeForCompare.getTime() >= new Date().getTime() - 1000 * 60 * 60 * 24) {
-      reservation.methods
-        .createReservation(placeId, reservationDate.toString(), time)
-        .send({ from: account })
-        .once("receipt", (receipt) => {
-          setReservations([receipt.events.ReservationCreated.returnValues]);
-          sendEmail();
-        });
+    if (checkIfTimeIsValid(date, time) && checkIfReservedAlready()) {
+      const reservationDate = date.toLocaleString();
+      const dateTimeForCompare = new Date(date);
+      if (account) {
+        if (dateTimeForCompare.getTime() >= new Date().getTime() - 1000 * 60 * 60 * 24) {
+          reservation.methods
+            .createReservation(placeId, reservationDate.toString(), time)
+            .send({ from: account })
+            .once("receipt", (receipt) => {
+              setReservations([receipt.events.ReservationCreated.returnValues]);
+              sendEmail();
+            });
+        }
+        else {
+          setError("Date is invalid!");
+          setSuccessMessage("");
+        }
+      }
+      else {
+        window.alert("Please connect MetaMask!");
+      }
     }
     else {
-      setError("Date is invalid!");
+      setSuccessMessage("");
     }
   }
 
@@ -178,15 +239,14 @@ const ReservationPage = ({ place, user, ...args }) => {
                     hour24
                   />
                 </div>
-
               </div>
               <button type='submit' className="login-btn btngreen">Book</button>
             </form>
             {error !== "" && <Alert style={{ marginTop: "10px", border: "10px" }} key="danger" variant="danger">
               {error}
             </Alert>}
-            {isReserved && <Alert style={{ marginTop: "30px", border: "10px" }} key="success" variant="success">
-              Reservation completed! Email sent.
+            {successMessage !== "" && <Alert style={{ marginTop: "30px", border: "10px" }} key="success" variant="success">
+              {successMessage}
             </Alert>}
           </div>
         </Col>
